daa9ad019d59251f5c4834ee223fa4cd56bf1ff7
vsoch <vsochat@stanford.edu>
2015-10-31 13:15:48 -0700

    modified:   circle.yml 	modified:   circle_urls.sh

--- circle_urls.sh
+++ circle_urls.sh
@@ -1,4 +1,5 @@
 BASEURL=https://circle-artifacts.com/gh/$CIRCLE_USERNAME/nipy.github.com/$CIRCLE_PREVIOUS_BUILD_NUM/artifacts/0/home/ubuntu/nipy.github.com/_site/
 echo $BASEURL
 sed -i "s,/,$BASEURL,g" "_config.yml"
+sed -i "14 s,.*,destination: ./_site,g" "_config.yml"
 sed -i "11 s,.*,baseurl: $BASEURL,g" "_config.yml"
