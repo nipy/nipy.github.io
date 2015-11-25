32bc5347c674217bca488a6fbcbc27df2d150c05
Ben Cipollini <bcipolli@ucsd.edu>
2015-11-25 10:18:08 -0500

    Update circle config; we should use baseurl everywhere.

--- circle_urls.sh
+++ circle_urls.sh
@@ -1,5 +1,3 @@
 BASEURL=https://circle-artifacts.com/gh/nipy/nipy.github.com/$CIRCLE_BUILD_NUM/artifacts/0/home/ubuntu/nipy.github.com/_site/
-echo $BASEURL
-sed -i "s,/,$BASEURL,g" "_config.yml"
 sed -i "14 s,.*,destination: ./_site,g" "_config.yml"
 sed -i "11 s,.*,baseurl: $BASEURL,g" "_config.yml"
