1270fe7f19c7cbe58e0b2a9deefb4420cbae8d60
Vanessa Sochat <vsochat@stanford.edu>
2019-02-06 12:07:25 -0500

    try updating BASEURL

--- circle_urls.sh
+++ circle_urls.sh
@@ -1,3 +1,3 @@
-BASEURL=https://circle-artifacts.com/gh/nipy/nipy.github.com/$CIRCLE_BUILD_NUM/artifacts/0/home/ubuntu/nipy.github.com/_site
+BASEURL=https://${CIRCLE_BUILD_NUM}-1283579-gh.circle-artifacts.com/0/nipy.github.com
 sed -i "14 s,.*,destination: ./_site,g" "_config.yml"
 sed -i "11 s,.*,baseurl: $BASEURL,g" "_config.yml"
